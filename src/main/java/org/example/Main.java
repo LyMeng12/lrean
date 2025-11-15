b38a65bdd38deaa4b3f9eccc2c159c363c910eef
Sao Lymeng <mengsaoly@gmail.com>
2025-11-15 21:49:42 +0700

    done game 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,9 +14,8 @@ public class Main {
             System.out.println("Welcome My game.");
             System.out.println("I have 2 game Need you choose.");
             System.out.println("1.Find My Number.");
-            System.out.println("2.Find My Name.");
+            System.out.println("2.Play card whit me.");
             System.out.println("=============================");
-//            boolean pp ;
             System.out.print("Choose:");
             int play = in.nextInt();
             while (play<1 || play>2){
@@ -45,7 +44,7 @@ public class Main {
     public static void game1(){
         Scanner in = new Scanner(System.in);
         System.out.println("This is game 1.");
-        System.out.println("1 - 10 i have 1 number");
+        System.out.println("(1 - 10) I have 1 number");
         System.out.println("i need you find this u have 2 point.");
         int random = (int)(Math.random()*10)+1;
         int num;
@@ -72,6 +71,52 @@ public class Main {
         }
     }
     public static void game2(){
-        System.out.println("hello game2");
+        Scanner in = new Scanner(System.in);
+        System.out.println("This is game 2.");
+        System.out.println("You Play card whit me.");
+        System.out.println("full Point 22 u have 2 and 3 card.");
+        System.out.println("if your point 20 me 22 me win.");
+        System.out.print("You can start new(Y/N):");
+        String play = in.next().toLowerCase();
+        while (play.equals("y")){
+            System.out.println("Admin:");
+            int card1admin = (int)(Math.random() * 10) + 1;
+            int card2admin = (int)(Math.random() * 10) + 1;
+            System.out.println(card1admin + " + " + card2admin + " = " + (card1admin + card2admin));
+
+
+
+
+            System.out.println("Me:");
+            int card1user = (int)(Math.random() * 10) + 1;
+            int card2user = (int)(Math.random() * 10) + 1;
+            System.out.println(card1user + " + " + card2user + " = " + (card1user + card2user));
+            System.out.print("You need 1 more?:");
+            String user = in.next().toLowerCase();
+            if(user.equals("y")){
+                System.out.println("Admin:");
+                int adminsum = card1admin + card2admin;
+                System.out.println(card1admin + " + " + card2admin + " = " + adminsum);
+
+
+
+
+                System.out.println("Me:");
+
+                int card3user = (int)(Math.random() * 10) + 1;
+                int usersum = card1user + card2user + card3user;
+                System.out.println(card1user + " + " + card2user + " + " + card3user + " = " + usersum);
+
+            }
+            if()
+            System.out.print("Play card again !(y/n):");
+            play = in.next().toLowerCase();
+            if (play.equals("y")){
+                play = "y";
+            }else if (play.equals("n")){
+                play = "n";
+            }
+        }
+
     }
 }
