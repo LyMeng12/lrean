e59ee814236d443f99adae1d32102a0a76886ac4
Sao Lymeng <mengsaolymeng@gmail.com>
2025-11-20 15:26:13 +0700

    array2

--- src/main/java/org/example/Oop.java
+++ src/main/java/org/example/Oop.java
@@ -47,30 +47,61 @@ public class Oop {
 //            System.out.print((sum));
 //            System.out.println();
 //        }
-        int[][] a = {{2, 3, 4},{5, 6, 7},{8, 9, 10}};
+        int[][] a = {{2, 3, 4},{5, 6, 7}};
+        int[][] b =new int[3][2];
 
         System.out.println("Welcome to the ArrayMulti program");
+
+        for (int i = 0; i < a.length; i++) {
+
+            for (int j = 0; j < a[i].length; j++) {
+//                System.out.print(a[i][j]);
+                b[j][i]=a[i][j];
+            }
+
+        }
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                System.out.print(b[i][j]);
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        for(int i=0;i<3; i++){
 //            for(int j=0 ;j<3; j++){
 //                System.out.print("Enter A:");
 //                a[i][j] = cin.nextInt();
 //            }
 //        }
-        System.out.println("==============================");
-        int bigRow=0;
-        for(int i=0;i<3;i++){
-            int big=a[i][0];
-            for(int j=0 ;j<3; j++){
-                if(big<a[i][j]){
-                    big= a[i][j];
-                }
-            }
-            if(big>bigRow){
-                bigRow=big;
-                System.out.println(bigRow);
-            }
-
-        }
-        System.out.println((bigRow));
+//        System.out.println("==============================");
+//        int bigRow=0;
+//        for(int i=0;i<3;i++){
+//            int big=a[i][0];
+//            for(int j=0 ;j<3; j++){
+//                if(big<a[i][j]){
+//                    big= a[i][j];
+//                }
+//            }
+//            if(big>bigRow){
+//                bigRow=big;
+//                System.out.println(bigRow);
+//            }
+//
+//        }
+//        System.out.println((bigRow));
     }
 }
